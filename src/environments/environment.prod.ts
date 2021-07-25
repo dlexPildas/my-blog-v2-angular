bb6b5eb65260f6e8dbdfdab22bdc11f5a41c4f11
Daniel Oliveira <oliveiradaniel11@hotmail.com>
2021-07-25 16:15:32 -0300

    add var at environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCy3YTT3slN-WHhbF52G25cnSJ1mlq3eXY",
+    authDomain: "blog-junior-forever.firebaseapp.com",
+    projectId: "blog-junior-forever",
+    storageBucket: "blog-junior-forever.appspot.com",
+    messagingSenderId: "996680124430",
+    appId: "1:996680124430:web:d36e34b2c46be02452945f",
+    measurementId: "G-M3B1LZCY2C"
+  }
 };
